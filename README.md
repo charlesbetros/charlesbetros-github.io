261577d22036f5bbb439b2c222d34ae066152d40
Charles Betros <charles.betros@gmail.com>
2019-01-21 17:09:34 -0600

    Working on output slides.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 https://developer.mozilla.org/en-US/docs/Web/API/console
 
-https://developers.google.com/web/tools/chrome-devtools/console/track-exceptions#print_stack_traces
+https://developers.google.com/web/tools/chrome-devtools/console
 
 https://medium.freecodecamp.org/how-to-get-the-most-out-of-the-javascript-console-b57ca9db3e6d
+
+https://medium.com/@mattburgess/beyond-console-log-2400fdf4a9d8
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+//add the router
+app.use("/", router);
+app.use("/lib/css", express.static(__dirname + "/lib/css"));
+app.use("/lib/js", express.static(__dirname + "/lib/js"));
+app.use("/plugin", express.static(__dirname + "/plugin"));
+app.use("/images", express.static(__dirname + "/images"));
+
+app.listen(process.env.port || 3000);
+
+console.log("Running at Port 3000");
