186b42c9d3b8da85dcd6b37ec9197cd5161dc8af
Charles Betros <charles.betros@gmail.com>
2019-01-21 21:00:01 -0600

    Move to console subdirectory.

--- console/lib/js/talk/tables.js
+++ console/lib/js/talk/tables.js
@@ -2,8 +2,8 @@
 //    Displays tabular data as a table.
 
 const people = [
-    { "Name": "Charles B", "Title": "Professional" },
-    { "Name": "Chuck D", "Title": "Public Enemy" }
+    { "Name": "Charles B", "Title": "Professional", "Skills": ["All the things"] },
+    { "Name": "Chuck D", "Title": "Public Enemy", "Skills": ["Musician"] }
 ];
 
 function doTable() {
